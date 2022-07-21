27ce14c553a7bde163b19c34c6224f90951aa323
mdalamin07 <mdalamin67378@gmail.com>
2022-07-21 18:35:55 +0600

    create etab tarif html page

--- js/index.js
+++ js/index.js
@@ -56,3 +56,47 @@ $(document).ready(function () {
 
 
 })
+
+// Light Slider Start Here
+
+$ = jQuery;
+$( document ).ready(function() {
+  $v_slider_options = {
+      gallery: true,
+      item: 1,
+      loop:true,
+      slideMargin: 0,
+      thumbItem: 3,
+      vThumbWidth:260,
+      galleryMargin: 10,
+      thumbMargin: 10,
+      vertical: true
+  };
+  v_slider = $('#lightSliderVertical').lightSlider($v_slider_options);
+
+  /* Fancybox & lightSlider Sync - Bug Fix */
+  $selector = '#lightSlider li:not(".clone") a';
+  $selector += ',#lightSliderVertical li:not(".clone") a';
+  $().fancybox({
+    selector : $selector,
+    backFocus : false, //The most important options for sync bug fix
+    buttons : [ 
+      'slideShow',
+      'share',
+      'zoom',
+      'fullScreen',
+      'thumbs',
+      'download',
+      'close'
+    ]
+  });
+});
+
+
+$( window ).resize(function() {
+		slider.destroy();
+		h_slider = $('#ocassions-slider').lightSlider(h_slider_options);
+});
+ 
+
+// Light Slider End Here
